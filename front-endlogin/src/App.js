b1a24d71ba5d57e8ef98c2547bacedd93d086136
Siddharth Datthathreya <marshtomp.1999@hotmail.com>
2020-07-27 14:52:43 +0530

    Login Page - React Router Login and Register

--- front-endlogin/src/App.js
+++ front-endlogin/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch,Route} from "react-router-dom";
 import Home from "./components/pages/Home"
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 export default function App(){
     return <div>
         <Router>
             <Switch>
-                <Route path={"/"} component={Home}/>
+                <Route exact path={"/"} component={Home}/>
+                <Route path={"/login"} component={Login}/>
+                <Route path={"/register"} component={Register}/>
             </Switch>
         </Router>
     </div>;
