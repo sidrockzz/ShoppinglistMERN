f613d4951a0ce003fc739436cb1215e16e436ef0
Siddharth Datthathreya <marshtomp.1999@hotmail.com>
2020-08-13 13:28:02 +0530

    Register Page with hooks

--- front-endlogin/src/components/auth/AuthOptions.js
+++ front-endlogin/src/components/auth/AuthOptions.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import {useHistory} from "react-router-dom";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import UserContext from "../../context/UserContext";
 
-export default function AuthOptions(){
-    //Array
+export default function AuthOptions() {
+    const { userData, setUserData } = useContext(UserContext);
+
     const history = useHistory();
 
-    const register = ()=> history.push("/register");
-    const login = ()=> history.push("/login");
+    const register = () => history.push("/register");
+    const login = () => history.push("/login");
+    const logout = () => {
+        setUserData({
+            token: undefined,
+            user: undefined,
+        });
+        localStorage.setItem("auth-token", "");
+    };
 
-    return(
-      <nav className={"auth-options"}>
-          <button onClick={register}>register</button>
-          <button onClick={login}>Log in</button>
-      </nav>
+    return (
+        <nav className="auth-options">
+            {userData.user ? (
+                <button onClick={logout}>Log out</button>
+            ) : (
+                <>
+                    <button onClick={register}>Register</button>
+                    <button onClick={login}>Log in</button>
+                </>
+            )}
+        </nav>
     );
 }
