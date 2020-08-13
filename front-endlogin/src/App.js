d134a0bdfdf3dd97115d8fb04b052413f923aff5
Siddharth Datthathreya <marshtomp.1999@hotmail.com>
2020-08-13 12:14:29 +0530

    User Context hooks

--- front-endlogin/src/App.js
+++ front-endlogin/src/App.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import {BrowserRouter as Router, Switch,Route} from "react-router-dom";
 import Home from "./components/pages/Home"
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Header from "./components/layout/Header";
+import UserContext from "./context/UserContext";
+import Axios from 'axios';
 
 import "./style.css";
 
 export default function App(){
-    return <div>
+    const [userData, setUserData] = useState({
+      token: undefined,
+      user: undefined,
+    });
+
+    useEffect(()=>{
+        const checkLoggedIn = async () => {
+            let token = localStorage.getItem("auth-token");
+            if(token==null){
+                localStorage.setItem("auth-token","");
+                token = "";
+            }
+            const tokenRes = await Axios.post(
+                "http://localhost:5000/users/tokenIsValid",
+                null,
+                {headers: {"x-auth-token":token}}
+            );
+                if (tokenRes.data) {
+                const userRes = await Axios.get("http://localhost:5000/users/", {
+                    headers: { "x-auth-token": token },
+                });
+                setUserData({
+                    token,
+                    user: userRes.data,
+                });
+            }
+        };
+    checkLoggedIn();
+    },[]);
+
+    return (
+        <>
         <Router>
+            <UserContext.Provider value={{userData, setUserData}}>
             <Header/>
             <Switch>
                 <Route exact path={"/"} component={Home}/>
                 <Route path={"/login"} component={Login}/>
                 <Route path={"/register"} component={Register}/>
             </Switch>
+            </UserContext.Provider>
         </Router>
-    </div>;
+    </>
+    );
 }
